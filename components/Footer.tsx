6e3cfb93980ccd617417e7c6cf512c9b5452104e
bspraveen17 <billasujith77@gmail.com>
2024-07-03 16:30:21 +0530

    Bug fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,13 +6,6 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50"
-        />
-      </div>
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">Contact Me</h1>
         <p className="text-white-200 md:mt-10 my-5">
@@ -38,12 +31,7 @@ const Footer = () => {
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
               <Link href={profile.link}>
-                <img
-                  src={profile.img}
-                  alt={profile.id}
-                  width={20}
-                  height={20}
-                />
+                <img src={profile.img} alt="profile" width={20} height={20} />
               </Link>
             </div>
           ))}
